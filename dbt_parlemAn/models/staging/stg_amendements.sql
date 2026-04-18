e863474f6da3cf148ac20a87f54e6a686d49c6c3
Boubacar Diallo <>
2026-04-18 13:35:46 +0000

    Fix Prefect an-dev deployment and make stg_amendements resilient when source table is missing

--- dbt_parlemAn/models/staging/stg_amendements.sql
+++ dbt_parlemAn/models/staging/stg_amendements.sql
@@ -1,5 +1,40 @@
 {{ config(materialized='view') }}
 
+{% set raw_relation = adapter.get_relation(
+  database=env_var('GCP_PROJECT'),
+  schema=env_var('BQ_DATASET'),
+  identifier='amendements'
+) %}
+
+{% if raw_relation is none %}
+  {{ log('raw_parleman.amendements is missing; stg_amendements will be an empty view for this run.', info=True) }}
+
+select
+  cast(null as string) as amendement_uid,
+  cast(null as string) as legislature,
+  cast(null as string) as numero_long,
+  cast(null as string) as numero_ordre_depot,
+  cast(null as string) as prefixe_organe_examen,
+  cast(null as string) as examen_ref,
+  cast(null as string) as texte_legislatif_ref,
+  cast(null as string) as dossier_legislatif_ref,
+  cast(null as string) as division_titre,
+  cast(null as string) as article_designation_courte,
+  cast(null as string) as division_type,
+  cast(null as date) as date_depot,
+  cast(null as date) as date_publication,
+  cast(null as string) as etat_code,
+  cast(null as string) as etat_libelle,
+  cast(null as string) as sous_etat_code,
+  cast(null as string) as sous_etat_libelle,
+  cast(null as string) as sort,
+  cast(null as string) as auteur_type,
+  cast(null as string) as auteur_acteur_ref,
+  cast(null as string) as auteur_groupe_politique_ref
+where false
+
+{% else %}
+
 select
   uid as amendement_uid,
   legislature,
@@ -25,3 +60,5 @@ select
   auteur_acteur_ref,
   auteur_groupe_politique_ref
 from {{ source('raw_parleman', 'amendements') }}
+
+{% endif %}
